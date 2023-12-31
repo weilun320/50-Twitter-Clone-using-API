cfe2a6b0be989d09f55a659ad5df33e35e04deec
Wei Lun <weilun9320@hotmail.com>
2023-12-31 21:16:48 +0800

    Added function to handle Sign up

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,11 +1,28 @@
+import axios from "axios";
 import { useState } from "react";
 import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap";
 
 export default function AuthPage() {
   const loginImage = "https://sig1.co/img-twitter-1";
+  const url = "https://48-restful-expressjs-pgsql-login-api.vercel.app";
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleOpen = () => setShow(true);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`${url}/signup`, { username, password });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Row className="gx-0">
@@ -39,28 +56,38 @@ export default function AuthPage() {
           <Button className="rounded-pill" variant="outline-primary">Sign In</Button>
         </Col>
         <Modal show={show} onHide={handleClose} centered>
-          <Modal.Body className="d-grid gap-2 px-5">
+          <Modal.Body>
             <h2 className="mb-4 fw-bold">
               Create your account
             </h2>
-            <Form>
+            <Form className="d-grid gap-2 px-5" onSubmit={handleSignUp}>
               <Form.Group className="mb-3" controlId="email">
-                <Form.Control placeholder="Enter email" type="email" />
+                <Form.Control
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Enter email"
+                  type="email"
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="password">
-                <Form.Control placeholder="Password" type="password" />
+                <Form.Control
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  type="password"
+                />
               </Form.Group>
-            </Form>
-            <p style={{ fontSize: 12 }}>
-              By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
-              Luna Tweets may use your contact information, including your email address and phone number
-              for purposes outlined in our Privacy Policy, like keeping you account secure and personalising
-              our services, including ads. Learn more. Others will be able to find you by email or phone
-              number, when provided, unless you choose otherwise here.
-            </p>
+              <p style={{ fontSize: 12 }}>
+                By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
+                Luna Tweets may use your contact information, including your email address and phone number
+                for purposes outlined in our Privacy Policy, like keeping you account secure and personalising
+                our services, including ads. Learn more. Others will be able to find you by email or phone
+                number, when provided, unless you choose otherwise here.
+              </p>
 
-            <Button className="rounded-pill">Sign Up</Button>
+              <Button className="rounded-pill" type="submit">
+                Sign Up
+              </Button>
+            </Form>
           </Modal.Body>
         </Modal>
       </Col>
