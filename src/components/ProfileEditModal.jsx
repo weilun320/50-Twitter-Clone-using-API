dd8d249bc3a43e1568156e5b47a216c6dc909682
Wei Lun <weilun9320@hotmail.com>
2024-01-04 17:30:50 +0800

    Add: handle save profile details function

--- src/components/ProfileEditModal.jsx
+++ src/components/ProfileEditModal.jsx
@@ -1,38 +1,158 @@
-import { useRef, useState } from "react";
-import { Button, FloatingLabel, Form, Image, Modal } from "react-bootstrap";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useRef, useState } from "react";
+import { Button, FloatingLabel, Form, Modal, Spinner } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { fetchUserDetails } from "../features/users/usersSlice";
 
-export default function ProfileEditModal({ show, handleClose }) {
+export default function ProfileEditModal({ show, handleClose, userDetails }) {
+  const BASE_URL = "https://b8b50c4b-de8f-426c-ad74-875a697d35e4-00-ppgcvyyh91fa.teams.replit.dev";
+
+  const [username, setUsername] = useState("");
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
-  const [imageFile, setImageFile] = useState(null);
+  const [profileImageFile, setProfileImageFile] = useState(null);
   const [profileImage, setProfileImage] = useState("");
+  const [isProfileImageUploaded, setIsProfileImageUploaded] = useState(false);
+  const [bannerImageFile, setBannerImageFile] = useState(null);
+  const [bannerImage, setBannerImage] = useState("");
+  const [isBannerImageUploaded, setIsBannerImageUploaded] = useState(false);
+
   const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const token = localStorage.getItem("authToken");
+  const decode = jwtDecode(token);
+  const userId = decode.id;
+
+  useEffect(() => {
+    if (userDetails) {
+      setUsername(userDetails.username ? userDetails.username : "");
+      setName(userDetails.name ? userDetails.name : "");
+      setBio(userDetails.bio ? userDetails.bio : "");
+      setProfileImage(userDetails.profileImage);
+      setBannerImage(userDetails.bannerImage);
+    }
+  }, [userDetails]);
 
   const profileImageInputRef = useRef();
+  const bannerImageInputRef = useRef();
 
   const handleProfileImageClick = () => {
     profileImageInputRef.current.click();
   }
 
-  const handleFileChange = (e) => {
+  const handleBannerImageClick = () => {
+    bannerImageInputRef.current.click();
+  }
+
+  const handleProfileImageChange = (e) => {
     const selectedFile = e.target.files[0];
     setErrorMessage("");
 
+    if (!selectedFile) {
+      return;
+    }
+
+    // Ensure uploaded file type is image
     const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
     if (!allowedTypes.includes(selectedFile?.type)) {
       setErrorMessage("Only JPEG, PNG and GIF images are allowed");
-      setImageFile(null);
+      setProfileImageFile(null);
 
       return;
     }
 
-    setImageFile(selectedFile);
+    setProfileImageFile(selectedFile);
     setProfileImage(URL.createObjectURL(selectedFile));
+    setIsProfileImageUploaded(true);
+  };
+
+  const handleBannerImageChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setErrorMessage("");
+
+    if (!selectedFile) {
+      return;
+    }
+
+    // Ensure uploaded file type is image
+    const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
+    if (!allowedTypes.includes(selectedFile?.type)) {
+      setErrorMessage("Only JPEG, PNG and GIF images are allowed");
+      setBannerImageFile(null);
+
+      return;
+    }
+
+    setBannerImageFile(selectedFile);
+    setBannerImage(URL.createObjectURL(selectedFile));
+    setIsBannerImageUploaded(true);
+  };
+
+  const handleSaveProfileDetails = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    setIsLoading(true);
+
+    // Ensure username is entered
+    if (!username) {
+      setErrorMessage("Username can't be empty.");
+      setIsLoading(false);
+      return;
+    }
+
+    // Ensure name is entered
+    if (!name) {
+      setErrorMessage("Name can't be empty.");
+      setIsLoading(false);
+      return;
+    }
+
+    const data = { username, name, bio, profileImage: profileImageFile, bannerImage: bannerImageFile };
+
+    try {
+      const res = await axios.post(`${BASE_URL}/users/profile/${userId}`, data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log(res.data);
+      setIsLoading(false);
+      // Reset all file input
+      setProfileImage(null);
+      setProfileImageFile(null);
+      setIsProfileImageUploaded(false);
+      setBannerImage(null);
+      setBannerImageFile(null);
+      setIsBannerImageUploaded(false);
+      // Close modal
+      handleClose();
+      // Rerender user's details
+      dispatch(fetchUserDetails(userId));
+    } catch (error) {
+      console.error(error);
+      // Set error message from API
+      if (error.response && error.response.status === 400) {
+        setErrorMessage(error.response.data.message);
+        setIsLoading(false);
+      }
+      else if (error.response && error.response.status === 500) {
+        setErrorMessage(error.response.data.error);
+        setIsLoading(false);
+      }
+      else {
+        setErrorMessage(error.message);
+        setIsLoading(false);
+      }
+    }
   };
 
   return (
     <Modal show={show} onHide={handleClose} centered>
-      <Form encType="multipart/form-data">
+      <Form encType="multipart/form-data" onSubmit={handleSaveProfileDetails}>
         <Modal.Header className="justify-content-start">
           <Button
             className="me-3 rounded-circle"
@@ -43,26 +163,70 @@ export default function ProfileEditModal({ show, handleClose }) {
             <i className="bi bi-x-lg"></i>
           </Button>
           <span className="fw-bold fs-4">Edit Profile</span>
-          <Button className="fw-semibold rounded-pill" variant="primary ms-auto">Save</Button>
+          <Button
+            className="fw-semibold rounded-pill"
+            type="submit"
+            variant="primary ms-auto"
+          >
+            {isLoading ? (
+              <>
+                <Spinner
+                  animation="border"
+                  as="span"
+                  className="me-2"
+                  size="sm"
+                />
+                <span>Loading...</span>
+              </>
+            ) : (
+              "Save"
+            )}
+          </Button>
         </Modal.Header>
         <Modal.Body>
-          <Form.Group className="mb-3" controlId="profile-image">
-            <div className="position-relative" style={{ width: "fit-content" }}>
-              <Image
-                className="object-fit-cover border"
-                height={100}
-                roundedCircle
-                src={profileImage ? profileImage : "./assets/default-profile-image.png"}
-                width={100}
+          <div className="position-relative w-100" style={{
+            backgroundBlendMode: "multiply",
+            backgroundColor: "#ccc",
+            backgroundImage: bannerImage && isBannerImageUploaded ? bannerImage && `url(${bannerImage})` : userDetails && userDetails.bannerImage && `url(${BASE_URL}/${bannerImage})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: 150,
+          }}>
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <Form.Control
+                accept="image/*"
+                onChange={handleBannerImageChange}
+                ref={bannerImageInputRef}
+                style={{ display: "none" }}
+                type="file"
               />
-              <div
-                className="position-absolute top-0 start-0 w-100 h-100 rounded-circle"
-                style={{ backgroundColor: "rgba(0, 0, 0, .2)" }}
-              ></div>
+              <Button
+                className="rounded-circle"
+                onClick={handleBannerImageClick}
+                style={{ backgroundColor: "rgba(0, 0, 0, .5)", height: 42, width: 42 }}
+                variant="dark"
+              >
+                <i className="bi bi-camera"></i>
+              </Button>
+            </div>
+            <div className="position-absolute rounded-circle" style={{
+              backgroundBlendMode: "multiply",
+              backgroundColor: "#ccc",
+              backgroundImage: profileImage && isProfileImageUploaded ? profileImage && `url(${profileImage})` : userDetails && userDetails.profileImage && `url(${BASE_URL}/${profileImage})`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              border: "3px solid #F8F9FA",
+              height: 100,
+              marginLeft: 14,
+              top: 90,
+              width: 100,
+            }}>
               <div className="position-absolute top-50 start-50 translate-middle">
                 <Form.Control
                   accept="image/*"
-                  onChange={handleFileChange}
+                  onChange={handleProfileImageChange}
                   ref={profileImageInputRef}
                   style={{ display: "none" }}
                   type="file"
@@ -77,8 +241,15 @@ export default function ProfileEditModal({ show, handleClose }) {
                 </Button>
               </div>
             </div>
-            {errorMessage && <p className="text-danger mb-0">{errorMessage}</p>}
-          </Form.Group>
+          </div>
+          <FloatingLabel className="mt-5 mb-3" controlId="username" label="Username">
+            <Form.Control
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              type="text"
+              value={username}
+            />
+          </FloatingLabel>
           <FloatingLabel className="mb-3" controlId="name" label="Name">
             <Form.Control
               onChange={(e) => setName(e.target.value)}
@@ -97,8 +268,9 @@ export default function ProfileEditModal({ show, handleClose }) {
               value={bio}
             />
           </FloatingLabel>
+          {errorMessage && <p className="text-danger">{errorMessage}</p>}
         </Modal.Body>
       </Form>
     </Modal>
-  )
+  );
 }
