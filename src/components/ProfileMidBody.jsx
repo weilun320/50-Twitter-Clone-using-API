3666e20d224c7829c0dee13f09ee63cb5b9526fc
Wei Lun <weilun9320@hotmail.com>
2024-01-04 17:26:31 +0800

    Update: Fetch user details from database through API

--- src/components/ProfileMidBody.jsx
+++ src/components/ProfileMidBody.jsx
@@ -1,14 +1,16 @@
-import { Button, Col, Image, Nav, Row, Spinner } from "react-bootstrap";
+import { Button, Col, Nav, Row, Spinner } from "react-bootstrap";
 import ProfilePostCard from "./ProfilePostCard";
 import ProfileEditModal from "./ProfileEditModal";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPostsByUser } from "../features/posts/postsSlice";
+import { fetchUserDetails } from "../features/users/usersSlice";
 
 export default function ProfileMidBody() {
   const url = "https://pbs.twimg.com/profile_banners/83072625/1602845571/1500x500";
   const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+  const BASE_URL = "https://b8b50c4b-de8f-426c-ad74-875a697d35e4-00-ppgcvyyh91fa.teams.replit.dev";
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -17,6 +19,10 @@ export default function ProfileMidBody() {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.posts);
   const loading = useSelector((state) => state.posts.loading);
+  const userDetails = useSelector((state) => state.users.userDetails && state.users.userDetails.message ? null : state.users.userDetails);
+  const userLoading = useSelector((state) => state.users.loading);
+
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
@@ -24,26 +30,40 @@ export default function ProfileMidBody() {
     if (token) {
       const decodedToken = jwtDecode(token);
       const userId = decodedToken.id;
+      setUsername(decodedToken.username);
       dispatch(fetchPostsByUser(userId));
+      dispatch(fetchUserDetails(userId));
     }
   }, [dispatch]);
 
   return (
     <>
       <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
-        <Image src={url} fluid />
+        <div className="position-relative w-100" style={{
+          backgroundBlendMode: "multiply",
+          backgroundColor: "#ccc",
+          backgroundImage: userDetails && userDetails.bannerImage && `url(${BASE_URL}/${userDetails.bannerImage})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          height: 180,
+        }}>
+        </div>
         <br />
-        <Image
-          src={pic}
-          roundedCircle
-          style={{
-            border: "4px solid #F8F9FA",
-            marginLeft: 15,
-            position: "absolute",
-            top: 140,
-            width: 150,
-          }}
-        />
+        <div className="position-absolute rounded-circle" style={{
+          backgroundBlendMode: "multiply",
+          backgroundColor: "#ccc",
+          backgroundImage: userDetails && userDetails.profileImage && `url(${BASE_URL}/${userDetails.profileImage})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          border: "4px solid #F8F9FA",
+          height: 150,
+          marginLeft: 15,
+          top: 120,
+          width: 150,
+        }}>
+        </div>
 
         <Row className="justify-content-end">
           <Col xs="auto">
@@ -57,18 +77,24 @@ export default function ProfileMidBody() {
           </Col>
         </Row>
 
-        <p className="mt-5 fw-bold" style={{ margin: 0, fontSize: 15 }}>
-          Haris
-        </p>
+        {userLoading ? (
+          <Spinner animation="border" className="ms-3 mt-3" variant="primary" />
+        ) : (
+          <>
+            <p className="mt-5 fw-bold" style={{ margin: 0 }}>
+              {userDetails ? userDetails.name : username.split("@")[0]}
+            </p>
 
-        <p style={{ marginBottom: 2 }}>@haris.samingan</p>
+            <p className="text-secondary" style={{ fontSize: 15 }}>
+              @{userDetails ? userDetails.username : username.split("@")[0]}
+            </p>
 
-        <p>I help people switch careers to be a software developer at sigmaschool.co</p>
-
-        <p>Entrepreneur</p>
+            <p>{userDetails && userDetails.bio}</p>
+          </>
+        )}
 
         <p>
-          <strong>271</strong> Following <strong>610</strong> Followers
+          <strong>271</strong> Following <strong> 610</strong> Followers
         </p>
 
         <Nav variant="underline" defaultActiveKey="/home" justify>
@@ -91,11 +117,11 @@ export default function ProfileMidBody() {
         {loading && (
           <Spinner animation="border" className="ms-3 mt-3" variant="primary" />
         )}
-        {posts.length > 0 && posts.map((post) => (
-          <ProfilePostCard key={post.id} content={post.content} postId={post.id} />
+        {posts.length > 0 && [...posts].reverse().map((post) => (
+          <ProfilePostCard key={post.id} post={post} userDetails={userDetails} />
         ))}
       </Col>
-      <ProfileEditModal show={show} handleClose={handleClose} />
+      <ProfileEditModal show={show} handleClose={handleClose} userDetails={userDetails} />
     </>
   );
 }
---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const BASE_URL = "https://b8b50c4b-de8f-426c-ad74-875a697d35e4-00-ppgcvyyh91fa.teams.replit.dev";
+
+// Async thunk for fetching specific user's details
+export const fetchUserDetails = createAsyncThunk(
+  "users/fetchDetails",
+  async (userId) => {
+    const res = await fetch(`${BASE_URL}/users/profile/${userId}`);
+
+    return res.json();
+  }
+);
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: { userDetails: null, loading: true },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserDetails.fulfilled, (state, action) => {
+      state.userDetails = action.payload;
+      state.loading = false;
+    });
+  },
+});
+
+export default usersSlice.reducer;
