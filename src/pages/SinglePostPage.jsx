78173647462a8c06e95c5323809a618a89953d25
Wei Lun <weilun9320@hotmail.com>
2024-01-11 15:46:01 +0800

    Update: changes on code logic to retrieve post ID from useParams, separate useEffect to check on token and to fetch post data

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -1,47 +1,81 @@
 import { Col } from "react-bootstrap";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ProfilePostCard from "../components/ProfilePostCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CommentCard from "../components/CommentCard";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCommentsByPost } from "../features/comments/commentsSlice";
+import { jwtDecode } from "jwt-decode";
 
 export default function SinglePostPage() {
-  const location = useLocation();
   const navigate = useNavigate();
 
-  const { post, userId } = location.state || {};
+  const postId = parseInt(useParams().postId);
 
-  const [views, setViews] = useState(post ? post.views : 0);
+  const [userId, setUserId] = useState(null);
+  const [post, setPost] = useState(null);
+  const [views, setViews] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const dispatch = useDispatch();
   const comments = useSelector((state) => state.comments.comments);
 
   useEffect(() => {
-    if (!location.state) {
-      navigate("/search");
+    const token = localStorage.getItem("authToken");
+
+    if (!token) {
+      navigate("/login");
       return;
     }
+    else {
+      try {
+        const decodedToken = jwtDecode(token);
+        setUserId(decodedToken.id);
+      } catch (error) {
+        navigate("/login");
+        return;
+      }
+    }
+  }, [navigate]);
 
-    if (post.user_id !== userId) {
-      axios.put(`${process.env.BASE_URL}/posts/views/${post.id}`, { views: post.views + 1 })
+  useEffect(() => {
+    if (userId && postId) {
+      fetch(`${process.env.BASE_URL}/posts/${postId}`)
         .then((res) => {
-          console.log(res.data);
-          setViews(res.data.views);
+          if (res.ok) {
+            return res.json().then((data) => {
+              setPost(data);
+
+              if (data.user_id !== userId) {
+                axios.put(`${process.env.BASE_URL}/posts/views/${data.id}`, { views: data.views + 1 })
+                  .then((res) => {
+                    console.log(res.data);
+                    setViews(res.data.views);
+                  })
+                  .catch((error) => console.error("Error: ", error));
+              }
+            });
+          }
+          else {
+            return res.json().then((data) => setErrorMessage(data.error));
+          }
         })
         .catch((error) => console.error("Error: ", error));
-    }
 
-    dispatch(fetchCommentsByPost(post.id));
-  }, [dispatch, location, navigate, post, userId]);
+      dispatch(fetchCommentsByPost(postId));
+    }
+  }, [dispatch, postId, userId]);
 
   return (
     <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
       <h3 className="my-3 mx-2">Post</h3>
-      <ProfilePostCard post={{ ...post, views }} clickable={false} />
+      {errorMessage && <p>{errorMessage}</p>}
+      {post &&
+        <ProfilePostCard post={{ ...post, views }} clickable={false} />
+      }
       {comments && comments.length > 0 && comments.map((comment) => (
-        <CommentCard key={comment.id} userId={userId} comment={comment} postId={post.id} />
+        <CommentCard key={comment.id} userId={userId} comment={comment} postId={postId} />
       ))}
     </Col>
   );
