9de41a0414162653be20a9159fd1fff3788052e4
Wei Lun <weilun9320@hotmail.com>
2024-01-11 15:48:13 +0800

    Add: search bar to search for user's name or username

--- src/pages/UsersToFollowPage.jsx
+++ src/pages/UsersToFollowPage.jsx
@@ -1,13 +1,27 @@
 import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
-import { Col } from "react-bootstrap";
+import { Col, Form, InputGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import UsersToFollowCard from "../components/UsersToFollowCard";
 
 export default function UsersToFollowPage() {
   const navigate = useNavigate();
+
   const [notFollowing, setNotFollowing] = useState([]);
   const [currentUserId, setCurrentUserId] = useState(null);
+  const [keyword, setKeyword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchNotFollowingUser = async (userId) => {
+    try {
+      const res = await fetch(`${process.env.BASE_URL}/follows/${userId}`);
+      const data = await res.json();
+
+      setNotFollowing(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
@@ -21,29 +35,52 @@ export default function UsersToFollowPage() {
         const userId = decode.id;
         setCurrentUserId(userId);
 
-        const fetchNotFollowingUser = async () => {
-          try {
-            const res = await fetch(`${process.env.BASE_URL}/follows/${userId}`);
-            const data = await res.json();
-
-            setNotFollowing(data);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-
-        fetchNotFollowingUser();
+        fetchNotFollowingUser(userId);
       } catch (error) {
         navigate("/login");
       }
     }
   }, [navigate]);
 
+  const handleSearchUser = () => {
+    setErrorMessage("");
+    setNotFollowing([]);
+
+    if (!keyword) {
+      fetchNotFollowingUser(currentUserId);
+      return;
+    }
+
+    fetch(`${process.env.BASE_URL}/search/users/${keyword}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json().then((data) => setNotFollowing(data));
+        }
+        else {
+          return res.json().then((data) => setErrorMessage(data.error));
+        }
+      })
+      .catch((error) => console.error("Error: ", error));
+  };
+
   return (
     <>
       <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
-        <h4 className="my-3">Who to follow</h4>
-        {notFollowing.length > 0 && notFollowing.map((user) =>
+        <h4 className="my-3">Connect</h4>
+        <InputGroup className="my-3">
+          <InputGroup.Text>
+            <i className="bi bi-search"></i>
+          </InputGroup.Text>
+          <Form.Control
+            onChange={(e) => setKeyword(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" ? handleSearchUser() : null}
+            placeholder="Search"
+            type="text"
+            value={keyword}
+          />
+        </InputGroup>
+        {errorMessage && <p>{errorMessage}</p>}
+        {notFollowing && notFollowing.length > 0 && notFollowing.map((user) =>
           <UsersToFollowCard key={user.id} userId={user.id} currentUserId={currentUserId} />
         )}
       </Col>
